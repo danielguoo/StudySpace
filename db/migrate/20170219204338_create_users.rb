1993b65b7a038d6b9e8eb2c828a45fa64587fd47
7ryanlo <7ryanlo@gmail.com>
2017-02-19 14:53:37 -0800

    edits

--- db/migrate/20170219204338_create_users.rb
+++ db/migrate/20170219204338_create_users.rb
@@ -1,9 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t| 
-	    t.string :name
+	    t.string :firstName
+	    t.string :lastName
 	    t.string :email
-	    t.string :password	
+	    t.string :password
+	    t.major :major
+	    t.gradYear :gradYear	
 	    t.timestamps
     end
   end
--- db/migrate/20170219204424_create_courses.rb
+++ db/migrate/20170219204424_create_courses.rb
@@ -1,8 +1,9 @@
 class CreateCourses < ActiveRecord::Migration[5.0]
   def change
     create_table :courses do |t|
-
-      t.timestamps
+    	t.string :name 
+    	has_many :users
+      	t.timestamps
     end
   end
 end
