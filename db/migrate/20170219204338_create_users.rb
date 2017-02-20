c2e93e2aa8c90d6cc981223a5a45083755fd056d
7ryanlo <7ryanlo@gmail.com>
2017-02-19 17:12:38 -0800

    arrays

--- db/migrate/20170219204338_create_users.rb
+++ db/migrate/20170219204338_create_users.rb
@@ -1,14 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t| 
-	    t.string :firstName
-	    t.string :lastName
-	    t.string :email
-	    t.string :password
+    	t.integer :userID
+	    t.string :firstName, :limit => 25
+	    t.string :lastName, :limit => 50
+	    t.string :email, :default => "", :null => false
+	    t.string :password :limit => 50, :null => false
 	    t.string :major
-	    t.integer :gradYear	
+	    t.integer :gradYear
+	    t.integer :courses, array: true, default: []	
 	    t.timestamps
     end
     #has_many :courses
+    
   end
 end
