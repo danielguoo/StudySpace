475a3bf0b2134df833fa74530c7eff8029eb354d
Austin <austindlee997@gmail.com>
2017-02-26 19:53:45 -0800

    pop

--- db/migrate/20170219204338_create_users.rb
+++ db/migrate/20170219204338_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
-    create_table :users do |t| 
+    create_table :users do |t|
     	t.integer :userID
 	    t.string :firstName, :limit => 25
 	    t.string :lastName, :limit => 50
@@ -8,10 +8,10 @@ class CreateUsers < ActiveRecord::Migration[5.0]
 	    t.string :password :limit => 50, :null => false
 	    t.string :major
 	    t.integer :gradYear
-	    t.integer :courses, array: true, default: []	
+	    t.integer :courses, array: true, default: []
 	    t.timestamps
     end
     #has_many :courses
-    
+
   end
 end
