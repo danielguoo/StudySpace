37b66033294c428178696166a216b8078d81a5c8
7ryanlo <7ryanlo@gmail.com>
2017-02-26 20:45:31 -0800

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   		if @user.save
   			flash[:success] = "Welcome to the Sample App!"
       		redirect_to @user
+      		
     	else
       		render 'new'
     	end
