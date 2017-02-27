0baed38e07aa2d44b77aff6f518edda224d3f78c
Austin <austindlee997@gmail.com>
2017-02-27 00:18:55 -0800

    register page and added colors

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,8 +2,11 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( LoginAndReg.css )
-Rails.application.config.assets.precompile += %w( people.css )
+#Rails.application.config.assets.precompile += %w( reg.css )
+#Rails.application.config.assets.precompile += %w( peopleView.css )
+#Rails.application.config.assets.precompile += %w( login.css )
+#Rails.application.config.assets.precompile += %w( profilepage.css )
+#Rails.application.config.assets.precompile += %w( searchPeople.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
