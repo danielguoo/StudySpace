6ed62c95cc93eeb437f5843936f703c41311a641
7ryanlo <7ryanlo@gmail.com>
2017-03-01 17:42:11 -0800

    updated front page login

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,15 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get '/register', to:'pages#register'
-  get '/home', to:'pages#home'
+  get '/home', to:'sessions#new'
   get '/search', to:'pages#search'
   get '/people', to:'pages#people'
   get '/profilepage', to:'pages#profilepage'
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 
 end
