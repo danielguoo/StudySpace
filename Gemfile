0c8c012e4af0ea0015599fb72923767394b7c867
7ryanlo <7ryanlo@gmail.com>
2017-03-07 00:03:17 -0800

    extra touch ups

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 gem 'bootstrap-sass', '3.2.0.2'
-
+gem 'paperclip', '~> 5.1'
 #image uploading here
 gem 'carrierwave', '~> 1.0'
 gem 'mini_magick', '~> 4.6', '>= 4.6.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
-  #get 'sessions/new'
-
-  #get 'static_pages/home'
-  #get 'users/new'
-
   root 'static_pages#home'
-  #get '/register', to:'pages#register'
-  get '/home', to:'pages#home'
-  #get '/search', to:'pages#search'
-  #get '/people', to:'pages#people'
-  #get '/profilepage', to:'pages#profilepage'
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get '/logout',  to: 'sessions#destroy'
-  resources :users
+
+  #following pages#<name> routes are useless and are intended to be mere placeholders only.
+  get '/search', to:'pages#search'
+  get '/people', to:'pages#people'
+  #get '/profilepage', to:'pages#profilepage'
+  
+
+  resources :users #do not delete this!!!
 
 end
