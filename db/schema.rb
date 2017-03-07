2effcf40b5246dadae2b73dd2b3af2835f687b85
7ryanlo <7ryanlo@gmail.com>
2017-03-07 00:34:55 -0800

    image upload added with paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302054204) do
+ActiveRecord::Schema.define(version: 20170307081043) do
 
   create_table "courses", force: :cascade do |t|
     t.integer  "courseID"
@@ -28,14 +28,18 @@ ActiveRecord::Schema.define(version: 20170302054204) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "password_digest"
     t.string   "major"
     t.integer  "gradyear"
     t.text     "bio"
     t.string   "facebook"
-    t.binary   "avatar"
+    #t.binary   "avatar"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
