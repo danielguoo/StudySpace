d85cc01d76debaa3a9ee6c613511541a1062c760
Daniel Guo <danielg216@gmail.com>
2017-03-07 23:35:19 -0800

    changed course and start posts

--- db/migrate/20170219204424_create_courses.rb
+++ db/migrate/20170219204424_create_courses.rb
@@ -2,6 +2,9 @@ class CreateCourses < ActiveRecord::Migration[5.0]
   def change
     create_table :courses do |t|
     	t.string :name
+    	t.string :subject
+    	t.string :professor
+    	t.text :description
       t.timestamps
     end
     #add_column("courses", "name")
