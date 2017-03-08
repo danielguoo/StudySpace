9e09b2a67ce7566198872638880df38514dcdba6
7ryanlo <7ryanlo@gmail.com>
2017-03-07 23:57:04 -0800

    edits

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170308070300) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
+    t.text     "content"
     t.datetime "published_at"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
