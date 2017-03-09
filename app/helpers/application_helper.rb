d9f4fb839127a1c606b3a05872fe064c6a1b9f1f
7ryanlo <7ryanlo@gmail.com>
2017-03-08 18:14:46 -0800

    Attempt to add Materialize to the courses page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,12 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  def avatar_exists?(user)
+  	if user.avatar.present?
+    	image_tag user.avatar.url
+  	else
+    	image_tag "/images/default-user.png"
+	end
+  end
 end
